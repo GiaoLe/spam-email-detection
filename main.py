2e3674f76740b35613446d58ba8ed40d1f9619aa
GiaoLe <giaolexuan104@gmail.com>
2024-04-25 20:04:05 +0700

    Add stop words removal and experimenting the code a bit

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import numpy as np
 import pandas as pd
-
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
 
 data = pd.read_csv('spam_ham_email_dataset.csv')
+
+stop_words = set(stopwords.words('english'))
+
+
+def remove_stop_words(sentence):
+    words = sentence.split()
+    words = [word for word in words if word not in stop_words]
+    return ' '.join(words)
 
 
 def get_sequences(texts, tokenizer, train=True, max_seq_length=None):
@@ -19,40 +29,33 @@ def get_sequences(texts, tokenizer, train=True, max_seq_length=None):
     return sequences
 
 
-def preprocess_inputs(df):
-    df = df.copy()
+df = data.copy()
 
-    # Split df into X and y
-    y = df['label']
-    y = y.replace({'ham': 0, 'spam': 1})
-    X = df['text']
+# Split df into X and y
+y = df['label']
+y = y.map({'ham': 0, 'spam': 1})
+X = df['text']
 
-    # Train-test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, shuffle=True, random_state=1)
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, shuffle=True, random_state=1)
 
-    # Create tokenizer
-    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=30000)
+# Create tokenizer
+tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=30000)
 
-    # Fit the tokenizer
-    tokenizer.fit_on_texts(X_train)
+# Fit the tokenizer
+tokenizer.fit_on_texts(X_train)
 
-    # Convert texts to sequences
-    X_train = get_sequences(X_train, tokenizer, train=True)
-    X_test = get_sequences(X_test, tokenizer, train=False, max_seq_length=X_train.shape[1])
+# Stop words removal
+X = pd.Series(X)
+X = X.apply(remove_stop_words)
 
-    return X_train, X_test, y_train, y_test
-
-
-X_train, X_test, y_train, y_test = preprocess_inputs(data)
-
-print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+# Convert texts to sequences
+X_train = get_sequences(X_train, tokenizer, train=True)
+X_test = get_sequences(X_test, tokenizer, train=False, max_seq_length=X_train.shape[1])
 
 inputs = tf.keras.Input(shape=(5029,))
 
-embedding = tf.keras.layers.Embedding(
-    input_dim=5000,
-    output_dim=64
-)(inputs)
+embedding = tf.keras.layers.Embedding(input_dim=5000, output_dim=64)(inputs)
 
 flatten = tf.keras.layers.Flatten()(embedding)
 
@@ -89,3 +92,5 @@ results = model.evaluate(X_test, y_test, verbose=0)
 print("    Test Loss: {:.4f}".format(results[0]))
 print("Test Accuracy: {:.2f}%".format(results[1] * 100))
 print("     Test AUC: {:.4f}".format(results[2]))
+
+# TODO: Matplotlib
